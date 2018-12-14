4022356a0a74caab7f1798e55b5097d4b6f8dbc1
VFrancishko <vfrancishko@rstyle.kiev.ua>
2018-12-14 17:51:32 +0200

    .

--- src/main/java/com/dev/doctor/workflow/model/User.java
+++ src/main/java/com/dev/doctor/workflow/model/User.java
@@ -1,4 +1,14 @@
 package com.dev.doctor.workflow.model;
 
+import com.dev.doctor.workflow.acces.Role;
+
+import java.util.Date;
+import java.util.UUID;
+
 public interface User {
+    int getId();
+    UUID getUUID();
+    boolean isLocked();
+    Date getDatelocked();
+    Role getUserRole();
 }
